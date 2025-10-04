ad96e3dfe936e4de7f70b88c589028f8a55cd76a
SNEHA SHAW <snehashaw0609@gmail.com>
2025-10-04 10:17:16 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3) //code readability should be high
+
+    console.log("sneha")
+
+    let name = "sneha"
+    let age = 18
+    let isLoggedIn = false
+
+    // number => 2 to power 53
+    // bigint
+    // string => ""
+    // boolean => true/false
+    // null => standalone value
+    // undefined =>
+    // symbol => unique
+
+    //object
+
+    console.log(typeof "sneha");
+    console.log(typeof undefined); //undefined
+    console.log(typeof null); //object
