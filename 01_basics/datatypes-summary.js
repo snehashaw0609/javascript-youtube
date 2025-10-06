36fc2d9cd008fc842446042b2740c3254f0ae3d9
SNEHA SHAW <snehashaw0609@gmail.com>
2025-10-06 09:08:19 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,31 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id===anotherId);
+
+const bigNumber = 345672663764n
+
+//Reference(Non Primitive)
+
+// Array, Objects, Functions
+
+const heros = {"shaktiman","naagraj","doga"};
+let myObj = {
+    name: "sneha"
+    age: 20
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
